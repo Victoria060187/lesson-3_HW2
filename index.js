82cc1f8177820f56e5ddc73c858fe9f5918a1fe7
Victoria Laputko <vikalaputko87@gmail.com>
2023-05-26 21:43:06 +0300

    add new folder and files

---
+++ index.js
@@ -0,0 +1,16 @@
+const input = +prompt('Enter a number from 1 to 4');
+if (input === 1) {
+  alert('Today is a good day to start something');
+}
+else if (input === 2) {
+    alert('See your friend today');
+}
+else if (input === 3) {
+    alert('Buy yourself something tasty today');
+}
+else if (input === 4) {
+    alert('Do your HW today');
+}
+else {
+    alert('Invalid number');
+}
